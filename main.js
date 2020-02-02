cc11e569c0c9d8db0aef6915e21d41f2d22dd758
lzil <liang.zhou.18@ucl.ac.uk>
2020-02-02 11:20:55 +0000

    remove jquery, trim photos

--- main.js
+++ main.js
@@ -2,38 +2,76 @@ var COLOR_LIGHT = '#fdfdfd'
 var COLOR_DARK = '#444'
 var COLOR_NAV = '#999'
 
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function(){ 
 
-	/*
-	name underline animation
-	*/
+    /*
+    body fades in
+    */
 
-	$('#nav-about').hover(function() {
-		$('.underName').css({'width': '160px', 'stroke-width': '4px'});
-	}, function() {
-		$('.underName').css({'width': '0px', 'stroke-width': '0px'});
-	});
+    var op = 0
+    var body_int = 0.05
+    var body_time = 7
+    var timer_body = setInterval(function() {
+        if (op >= 1 - body_int) {
+            clearInterval(timer_body)
+            document.body.style.opacity = 1
+        }
+        op += body_int
+        document.body.style.opacity = op
+    }, body_time)
+
 
 	/*
 	pressing navbar button effects
 	*/
 
 	// setting initial 'about'. cur_target is the current display
-	var cur_target = $('#about');
-	cur_target.css('display', 'block');
+	var cur_target = document.querySelector('#about')
+	cur_target.style.display = 'block'
+    cur_target.style.opacity = 1
 	
-	$('.nav-link').click(function() {
-		var target = $('#'+this.id.slice(4))
-		if (target[0].id == cur_target[0].id) {
-			return
-		}
-		$(cur_target).fadeOut(160, function() {
-			target.fadeIn(160)
-			cur_target = target;
-		})
-		$('.nav-link').css('color', '')
-		$(this).css('color', COLOR_DARK)
-	})
+	document.querySelectorAll('.nav-link').forEach(function(l) {
+        l.addEventListener('click', function() {
+    		var target = document.querySelector('#'+this.id.slice(4))
+    		if (target.id == cur_target.id) {
+    			return
+    		}
+
+            // fading the sections in and out
+            // opacity decreases in increments of int_int, then increases
+            var op1 = 1
+            var op2 = 0
+            var int_time = 5
+            var int_int = 0.05
+            // timer1,op1 for leaving element, timer2,op2 for incoming element
+            var timer1 = setInterval(function() {
+                if (op1 <= int_int){
+                    clearInterval(timer1);
+                    cur_target.style.display = 'none'
+                    target.style.display = 'block'
+                    var timer2 = setInterval(function() {
+                        if (op2 >= 1 - int_int) {
+                            clearInterval(timer2)
+                            target.style.opacity = 1
+                            cur_target.style.opacity = 0
+                            cur_target = target
+                        }
+                        op2 += int_int
+                        target.style.opacity = op2
+                    }, int_time);
+                }
+                op1 -= int_int
+                cur_target.style.opacity = op1
+            }, int_time);
+
+            
+            document.querySelectorAll('.nav-link').forEach(function(m) {
+                m.style.color = ''
+            })
+
+    		this.style.color = COLOR_DARK
+	   })
+    })
 
 	/*
 	photo organization
@@ -42,13 +80,13 @@ $(document).ready(function() {
 	var load_photos = function(type) {
 		var photos = {}
 		photos[type] = shuffle(PHOTOS[type])
-		var photo_tab = $('.photo-tab.' + type)[0]
+		var photo_tab = document.querySelector('.photo-tab.' + type)
 
 		for (var i = 0; i < photos[type].length; i++) {
 
 			var photo = document.createElement("img");
 			var src = 'images/photos/' + photos[type][i];
-			$(photo).addClass('photo')
+			photo.classList.add('photo')
 			photo.src = src;
 
 			// add the photo and add the link
@@ -64,7 +102,8 @@ $(document).ready(function() {
 	load_photos("cities")
 	load_photos("misc")
 
-})
+}, false)
+
 
 
 // adapted from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
@@ -79,3 +118,4 @@ function shuffle(a) {
     }
     return b;
 }
+
